6d6aaed74cf4fbff8c411009752cf3a99ba19ce2
Dlzheen Mahmood <dlzheenmahmood@gmail.com>
2021-05-31 11:11:35 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -1,8 +1,8 @@
 
-
+let counter = document.querySelector("#counter");
 let plus = document.getElementById('plus');
    plus.addEventListner("click", function(){
-     
+     counter.innerHtml = counter.innerHtml+1;
    });
 function decrement() {
       document.getElementById('minus').stepDown();
